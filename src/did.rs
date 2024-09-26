3cebef871a956350c193352f1c1599e3d067e30c
Ravindu Wijegunawardhana <ravindu.w@gmail.com>
2024-09-26 12:13:59 +0530

    wasm impl

--- src/did.rs
+++ src/did.rs
@@ -1,27 +1,38 @@
-use serde::{Deserialize, Serialize};
-
 use crate::{
     error::Error,
     keys::{KeyPair, VerificationKey},
 };
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::wasm_bindgen;
+#[cfg(feature = "wasm")]
+use wasm_bindgen::JsValue;
 
 pub const DID_CONTEXT_URL: &str = "https://www.w3.org/ns/did/v1";
 
 #[derive(Serialize, Deserialize, Debug)]
+#[wasm_bindgen]
 pub struct DidDocument {
     #[serde(rename = "@context")]
+    #[wasm_bindgen(skip)]
     pub context: Vec<String>,
+    #[wasm_bindgen(skip)]
     pub id: String,
+    #[wasm_bindgen(skip)]
     #[serde(rename = "verificationMethod")]
     pub verification_method: Vec<KeyPair>,
+    #[wasm_bindgen(skip)]
     pub authentication: Vec<String>,
+    #[wasm_bindgen(skip)]
     #[serde(rename = "assertionMethod")]
     pub assertion_method: Vec<String>,
+    #[wasm_bindgen(skip)]
     #[serde(rename = "capabilityDelegation")]
     pub capability_delegation: Vec<String>,
+    #[wasm_bindgen(skip)]
     #[serde(rename = "capabilityInvocation")]
     pub capability_invocation: Vec<String>,
     #[serde(rename = "keyAgreement")]
+    #[wasm_bindgen(skip)]
     pub key_agreement: Vec<KeyPair>,
 }
 
@@ -32,8 +43,10 @@ pub trait KeyPairToDidDocument {
     ) -> Result<DidDocument, Error>;
 }
 
+#[wasm_bindgen]
 impl DidDocument {
-    pub fn get_key(&self, key_id_fragment: &str) -> Result<KeyPair, Error> {
+    #[wasm_bindgen(js_name = "getKeyPair")]
+    pub fn get_key_pair(&self, key_id_fragment: &str) -> Result<KeyPair, Error> {
         let key_id = format!("{}#{}", self.id, key_id_fragment);
 
         let v_id = self.verification_method[0].id.clone();
@@ -46,5 +59,107 @@ impl DidDocument {
         }
 
         Ok(public_key)
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(js_name = "toObject")]
+    pub fn to_object(&self) -> Result<JsValue, serde_wasm_bindgen::Error> {
+        serde_wasm_bindgen::to_value(self)
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(getter)]
+    pub fn context(&self) -> Vec<String> {
+        self.context.clone()
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(setter)]
+    pub fn set_context(&mut self, context: Vec<String>) {
+        self.context = context;
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(getter)]
+    pub fn id(&self) -> String {
+        self.id.clone()
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(setter)]
+    pub fn set_id(&mut self, id: String) {
+        self.id = id;
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(setter, js_name = "verificationMethod")]
+    pub fn set_verification_method(&mut self, verification_method: Vec<KeyPair>) {
+        self.verification_method = verification_method;
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(getter, js_name = "verificationMethod")]
+    pub fn verification_method(&self) -> Vec<KeyPair> {
+        self.verification_method.clone()
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(setter)]
+    pub fn set_authentication(&mut self, authentication: Vec<String>) {
+        self.authentication = authentication;
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(getter)]
+    pub fn authentication(&self) -> Vec<String> {
+        self.authentication.clone()
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(setter, js_name = "assertionMethod")]
+    pub fn set_assertion_method(&mut self, assertion_method: Vec<String>) {
+        self.assertion_method = assertion_method;
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(getter, js_name = "assertionMethod")]
+    pub fn assertion_method(&self) -> Vec<String> {
+        self.assertion_method.clone()
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(setter, js_name = "capabilityDelegation")]
+    pub fn set_capability_delegation(&mut self, capability_delegation: Vec<String>) {
+        self.capability_delegation = capability_delegation;
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(getter, js_name = "capabilityDelegation")]
+    pub fn capability_delegation(&self) -> Vec<String> {
+        self.capability_delegation.clone()
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(setter, js_name = "capabilityInvocation")]
+    pub fn set_capability_invocation(&mut self, capability_invocation: Vec<String>) {
+        self.capability_invocation = capability_invocation;
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(getter, js_name = "capabilityInvocation")]
+    pub fn capability_invocation(&self) -> Vec<String> {
+        self.capability_invocation.clone()
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(setter, js_name = "keyAgreement")]
+    pub fn set_key_agreement(&mut self, key_agreement: Vec<KeyPair>) {
+        self.key_agreement = key_agreement;
+    }
+
+    #[cfg(feature = "wasm")]
+    #[wasm_bindgen(getter, js_name = "keyAgreement")]
+    pub fn key_agreement(&self) -> Vec<KeyPair> {
+        self.key_agreement.clone()
     }
 }
--- src/keys.rs
+++ src/keys.rs
@@ -1,20 +1,121 @@
-use serde::{Deserialize, Serialize};
-
 use crate::error::Error;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::wasm_bindgen;
+#[cfg(feature = "wasm")]
+use wasm_bindgen::JsValue;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
+#[wasm_bindgen]
 pub struct KeyPair {
+    #[wasm_bindgen(skip)]
     pub id: Option<String>,
     #[serde(rename = "type")]
+    #[wasm_bindgen(skip)]
     pub _type: String,
     #[serde(rename = "@context")]
+    #[wasm_bindgen(skip)]
     pub context: Option<String>,
+    #[wasm_bindgen(skip)]
     pub public_key_base58: Option<String>,
+    #[wasm_bindgen(skip)]
     pub private_key_base58: Option<String>,
+    #[wasm_bindgen(skip)]
     pub public_key_multibase: Option<String>,
+    #[wasm_bindgen(skip)]
     pub private_key_multibase: Option<String>,
+    #[wasm_bindgen(skip)]
     pub revoked: bool,
+    #[wasm_bindgen(skip)]
     pub controller: Option<String>,
+}
+
+#[wasm_bindgen]
+#[cfg(feature = "wasm")]
+impl KeyPair {
+    #[wasm_bindgen(getter)]
+    pub fn id(&self) -> Option<String> {
+        self.id.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_id(&mut self, id: Option<String>) {
+        self.id = id;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn _type(&self) -> String {
+        self._type.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_type(&mut self, _type: String) {
+        self._type = _type;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn context(&self) -> Option<String> {
+        self.context.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_context(&mut self, context: Option<String>) {
+        self.context = context;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn public_key_base58(&self) -> Option<String> {
+        self.public_key_base58.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_public_key_base58(&mut self, public_key_base58: Option<String>) {
+        self.public_key_base58 = public_key_base58;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn private_key_base58(&self) -> Option<String> {
+        self.private_key_base58.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_private_key_base58(&mut self, private_key_base58: Option<String>) {
+        self.private_key_base58 = private_key_base58;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn public_key_multibase(&self) -> Option<String> {
+        self.public_key_multibase.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_public_key_multibase(&mut self, public_key_multibase: Option<String>) {
+        self.public_key_multibase = public_key_multibase;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn private_key_multibase(&self) -> Option<String> {
+        self.private_key_multibase.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_private_key_multibase(&mut self, public_key_multibase: Option<String>) {
+        self.public_key_multibase = public_key_multibase;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn controller(&self) -> Option<String> {
+        self.controller.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_controller(&mut self, controller: Option<String>) {
+        self.controller = controller;
+    }
+
+    #[wasm_bindgen(js_name = "toObject")]
+    pub fn to_object(&self) -> Result<JsValue, serde_wasm_bindgen::Error> {
+        serde_wasm_bindgen::to_value(self)
+    }
 }
 
 pub trait VerificationKey {
